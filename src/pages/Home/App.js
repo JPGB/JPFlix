2fc78d9d265a3f71ac3fd8b067c8b1884efb68fc
JP <joao.galindo@hotmail.com.br>
2020-07-29 12:16:15 -0300

    aula 2 roteamento e state

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Menu from './components/Menu';
-import DadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import DadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
+      
       <Menu />
+
       <BannerMain 
         videoTitle={DadosIniciais.categorias[0].videos[0].titulo}
         url={DadosIniciais.categorias[0].videos[0].url}
@@ -44,9 +46,11 @@ function App() {
         ignoreFirstVideo
         category={DadosIniciais.categorias[5]}
       />
+
+      <Footer />
       
     </div>
   );
 }
 
-export default App;
+export default Home;
